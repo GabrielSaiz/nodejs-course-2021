44df19e9a2a6623c51e4832325dd241ddd31c230
Gabriel Saiz <grsm.work@gmail.com>
2021-09-03 13:48:21 +0200

    Menu de consola

---
+++ 04-tareas-consola/helpers/mensajes.js
@@ -0,0 +1,57 @@
+require('colors');
+
+const { resolve } = require('path');
+
+
+const mostrarMenu = () => {
+
+    return new Promise(resolve => {
+
+        console.clear();
+        console.log('=============================='.green);
+        console.log('   Selecciona una opción: '.green);
+        console.log('=============================='.green);
+
+        console.log(` `);
+        console.log(`${'1.'.green} Crear tarea `);
+        console.log(`${'2.'.green} Listar tareas `);
+        console.log(`${'3.'.green} Listar tareas completadas `);
+        console.log(`${'4.'.green} Listar tareas pendientes `);
+        console.log(`${'5.'.green} Completar tarea(s) `);
+        console.log(`${'6.'.green} Borrar tarea `);    
+        console.log(`${'0.'.green} Salir `);
+        console.log(` `);
+
+
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        readLine.question('Seleccione una opción: ', (opt) => {
+            readLine.close();
+            resolve(opt)
+        })
+    })
+}
+
+const pausa = () => {
+    return new Promise (resolve => {
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        readLine.question(`\nPresione ${'ENTER'.green} para continuar\n`, () => {
+            
+            readLine.close();
+            resolve();
+        })
+    })
+}
+
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}
