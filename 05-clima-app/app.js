d2eec5b28ef1642d286f1e522eeb1bc16f89750f
Gabriel Saiz <saiz@xxxlgroup.com>
2021-09-09 18:13:02 +0200

    69. Modelo para controlar la aplicación

--- 05-clima-app/app.js
+++ 05-clima-app/app.js
@@ -1,12 +1,39 @@
-const { inquirerMenu, inquirerPausa } = require('./helpers/inquirer');
+const {
+  inquirerMenu,
+  inquirerPausa,
+  inquirerLeerInput
+} = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 console.log('APP CLIMA STARTING');
 
 // console.clear();
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt = '';
   do {
     opt = await inquirerMenu();
+
+    switch (opt) {
+      case '1':
+        // Mostrar Mensaje
+        const lugar = await inquirerLeerInput('Lugar: ');
+        console.log(lugar);
+        // Buscar los lugares
+        // Seleccionar el lugar
+        // Obtener clima
+        // Mostrar resultados
+        console.log('\nInformacion de la ciudad\n'.green);
+        console.log(`${'Ciudad:'.bold}`);
+        console.log(`${'Latitud:'.bold}`);
+        console.log(`${'Longitud:'.bold}`);
+        console.log(`${'Temperatura:'.bold}`);
+        console.log(`${'Mínima:'.bold}`);
+        console.log(`${'Máxima:'.bold}`);
+        break;
+      case '2':
+        break;
+    }
 
     if (opt !== '0') {
       await inquirerPausa();
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas {
+  historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  async ciudad(lugar = '') {
+    // peticion http
+    console.log(lugar);
+
+    return []; //devuelve las ciudades
+  }
+}
+
+module.exports = Busquedas;
